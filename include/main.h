bb9ac97ca521aadddc294ce6613834c85a05c835
sendsay <sendsay@yahoo.com>
2020-03-29 20:53:53 +0300

    Added ntpTime

--- include/main.h
+++ include/main.h
@@ -11,9 +11,27 @@
 
 const char* ssid = "PUTIN UTELE";
 const char* password = "0674788273";
-String ssidAP      = "Aquarium-AP";      // Назва точки доступу
-String passwordAP  = "" ;               // Пароль точки доступу
+const char* ssidAP      = "Aquarium-AP";         // Назва точки доступу
+const char* passwordAP  = "" ;                   // Пароль точки доступу
 
-boolean firstStart = false;                  // Первый старт
-boolean WIFI_connected = false;           // Флаг подкючекния к ВАЙФАЙ
-byte amountNotStarts = 0;                 // Счет НЕ подключения
+boolean firstStart = false;                 // Первый старт
+boolean WIFI_connected = false;             // Флаг подкючекния к ВАЙФАЙ
+byte amountNotStarts = 0;                   // Счет НЕ подключения
+
+//time
+long localEpoc = 0;
+long localMillisAtUpdate = 0;
+int hour=22, minute=40, second=42, month=4, day=6, dayOfWeek=6, year=2018;
+unsigned int localPort = 2390;
+bool statusUpdateNtpTime = 0;             // Если не 0, то обновление было удачным
+float timeZone = 2.0;                     // Временная зона для часов
+long timeUpdate = 60000;                  // Период обновления времени
+uint8_t hourTest[3], minuteTest[3];       // ??
+int g_hour, g_minute, g_second, g_month=1, g_day, g_dayOfWeek, g_year;  // ??
+const char* ntpServerName = "ntp3.time.in.ua";   // Сервер обновления времени
+const int NTP_PACKET_SIZE = 48;           // Размер пакета от сервера времени
+byte packetBuffer[NTP_PACKET_SIZE];       // Буфер пакета времени
+// float timeZone;                                     // Временная зона для часов
+int summertime = 1;                                     // летнее время
+#define LEAP_YEAR(Y) (((1970+Y)>0) && !((1970+Y)%4) && (((1970+Y)%100)||!((1970+Y)%400)))   // Високосні літа
+static const uint8_t monthDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};        // Кількість днів у місяцях
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
 
 /*
 ..#######..########........##.########..######..########..######.
@@ -16,8 +17,11 @@
 ..#######..########...######..########..######.....##.....######.
 */
 
-ESP8266WebServer server(80);          // web server
+ESP8266WebServer server(80);            // web server
 IPAddress apIP(192, 168, 10, 10);       // Adress AP
+WiFiUDP ntpUDP;                         // UDP client for time
+IPAddress timeServerIP;                 // server IP adress
+
 
 /*
 .########..########...#######...######..########.########..##.....##.########..########..######.
@@ -37,6 +41,10 @@ void styleaqua();                       // web style aqua css
 void aquapng();                         // web aqua png
 void lightpng();                        // web light png
 void parampng();                        // web param png
+void printTime();                       // print time for debug
+void timeUpdateNTP();                   // update time from iNet
+void getNTPtime();                      // get time from server
+
 
 /*
 ..######..########.########.##.....##.########.
@@ -58,13 +66,13 @@ void setup() {
 
     wifiConnect();                      // try connect to Wifi
 
-    // localMillisAtUpdate = millis();
-    // localEpoc = (hour * 60 * 60 + minute * 60 + second);
+    localMillisAtUpdate = millis();
+    localEpoc = (hour * 60 * 60 + minute * 60 + second);
 
-    // if(WiFi.status() == WL_CONNECTED) {
-    //     ntpUDP.begin(localPort);            // Запуск UDP для получения времени
-    //     timeUpdateNTP();                    // Обновление времени
-    // }
+    if(WiFi.status() == WL_CONNECTED) {
+        ntpUDP.begin(localPort);            // Запуск UDP для получения времени
+        timeUpdateNTP();                    // Обновление времени
+    }
 
     server.begin();
     server.on("/", fileindex);
@@ -72,7 +80,6 @@ void setup() {
     server.on("/aqua.html", fileaqua);
     server.on("style.css", style);
     server.on("/style.css", style);
-
     server.on("style-aqua.css", styleaqua);
     server.on("/style-aqua.css", styleaqua);
     server.on("aqua.png", aquapng);
@@ -82,11 +89,7 @@ void setup() {
     server.on("params.png", parampng);
     server.on("/params.png", parampng);
 
-
-
     SPIFFS.begin();
-
-
 }
 
 /*
@@ -101,9 +104,6 @@ void setup() {
 
 void loop() {
     server.handleClient();      // web server
-
-
-
 }
 
 /*
@@ -116,8 +116,17 @@ void loop() {
 .##........#######..##....##..######.....##....####..#######..##....##..######.
 */
 
+/*
+.##......##.####.########.####.....######...#######..##....##.##....##.########..######..########
+.##..##..##..##..##........##.....##....##.##.....##.###...##.###...##.##.......##....##....##...
+.##..##..##..##..##........##.....##.......##.....##.####..##.####..##.##.......##..........##...
+.##..##..##..##..######....##.....##.......##.....##.##.##.##.##.##.##.######...##..........##...
+.##..##..##..##..##........##.....##.......##.....##.##..####.##..####.##.......##..........##...
+.##..##..##..##..##........##.....##....##.##.....##.##...###.##...###.##.......##....##....##...
+..###..###..####.##.......####.....######...#######..##....##.##....##.########..######.....##...
+*/
 void wifiConnect() {
-    // printTime();
+    printTime();
     DEBUG("Connecting WiFi (ssid=" + String(ssid) + "  pass=" + String(password) + ") ");
 
 #ifdef DEBUG_ENABLE
@@ -170,9 +179,7 @@ void wifiConnect() {
         WiFi.mode(WIFI_AP);
         WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
         WiFi.softAP(ssidAP, passwordAP);
-
-        // printTime();
-
+        printTime();
         DEBUG("Start AP mode!!!");
         DEBUG("Wifi AP IP : ");
         DEBUG(WiFi.softAPIP());
@@ -183,11 +190,20 @@ void wifiConnect() {
     }
 }
 
+/*
+.##......##.########.########.
+.##..##..##.##.......##.....##
+.##..##..##.##.......##.....##
+.##..##..##.######...########.
+.##..##..##.##.......##.....##
+.##..##..##.##.......##.....##
+..###..###..########.########.
+*/
+
 void fileindex() {
     File file = SPIFFS.open("/index.html.gz", "r");
     size_t sent = server.streamFile(file, "text/html");
     DEBUG("load index page");
-
 }
 
 void fileaqua() {
@@ -206,7 +222,6 @@ void styleaqua() {
     File file = SPIFFS.open("/style-aqua.css.gz", "r");
     size_t sent = server.streamFile(file, "text/css");
     DEBUG("load aqua style css");
-
 }
 
 void aquapng() {
@@ -224,3 +239,154 @@ void parampng() {
     size_t sent = server.streamFile(file, "image/png");
     DEBUG("load param png");
 }
+
+/*
+.##.....##.########..########.....###....########.########.########.####.##.....##.########
+.##.....##.##.....##.##.....##...##.##......##....##..........##.....##..###...###.##......
+.##.....##.##.....##.##.....##..##...##.....##....##..........##.....##..####.####.##......
+.##.....##.########..##.....##.##.....##....##....######......##.....##..##.###.##.######..
+.##.....##.##........##.....##.#########....##....##..........##.....##..##.....##.##......
+.##.....##.##........##.....##.##.....##....##....##..........##.....##..##.....##.##......
+..#######..##........########..##.....##....##....########....##....####.##.....##.########
+*/
+void timeUpdateNTP() {
+    if(!WIFI_connected) return;
+    printTime();
+    statusUpdateNtpTime = 1;
+    for(int timeTest = 0; timeTest < 3; timeTest++) {
+        getNTPtime();
+
+        DEBUG("Proba #"+String(timeTest+1)+"   "+String(g_hour)+":"+((g_minute<10)?"0":"")+String(g_minute)+":"+((g_second<10)?"0":"")+String(g_second)+":"+String(g_dayOfWeek)+":"+String(g_day)+":"+String(g_month)+":"+String(g_year));
+
+        hourTest[timeTest] = g_hour;
+        minuteTest[timeTest] = g_minute;
+        if(statusUpdateNtpTime == 0) {
+            printTime();
+            DEBUG("ERROR TIME!!!\r\n");
+                return;
+        }
+        if(timeTest > 0) {
+            if((hourTest[timeTest] != hourTest[timeTest - 1]||minuteTest[timeTest] != minuteTest[timeTest - 1])) {
+                statusUpdateNtpTime = 0;
+                printTime();
+                DEBUG("ERROR TIME!!!\r\n");
+                return;
+            }
+        }
+    }
+
+    hour=g_hour;
+    minute=g_minute;
+    second=g_second;
+    day=g_day;
+    dayOfWeek=g_dayOfWeek;
+    month=g_month;
+    year=g_year;
+
+    localMillisAtUpdate = millis();
+    localEpoc = (hour * 60 * 60 + minute * 60 + second);
+
+    printTime();
+
+    DEBUG((day < 10 ? "0" : "") + String(day) + "." + (month < 10 ? "0" : "") + String(month) + "." + String(year) + " DW = " + String(dayOfWeek));
+    DEBUG("Time update OK.");
+}
+
+/*
+.########..########..####.##....##.########.########.####.##.....##.########
+.##.....##.##.....##..##..###...##....##.......##.....##..###...###.##......
+.##.....##.##.....##..##..####..##....##.......##.....##..####.####.##......
+.########..########...##..##.##.##....##.......##.....##..##.###.##.######..
+.##........##...##....##..##..####....##.......##.....##..##.....##.##......
+.##........##....##...##..##...###....##.......##.....##..##.....##.##......
+.##........##.....##.####.##....##....##.......##....####.##.....##.########
+*/
+void printTime() {
+    DEBUG((hour < 10 ? "0" : "") + String(hour) + ":" + (minute < 10 ? "0" : "") + String(minute) + ":" + (second < 10 ? "0" : "") + String(second) + "  ");
+}
+
+/*
+..######...########.########.##....##.########.########..########.####.##.....##.########
+.##....##..##..........##....###...##....##....##.....##....##.....##..###...###.##......
+.##........##..........##....####..##....##....##.....##....##.....##..####.####.##......
+.##...####.######......##....##.##.##....##....########.....##.....##..##.###.##.######..
+.##....##..##..........##....##..####....##....##...........##.....##..##.....##.##......
+.##....##..##..........##....##...###....##....##...........##.....##..##.....##.##......
+..######...########....##....##....##....##....##...........##....####.##.....##.########
+*/
+void getNTPtime() {
+
+        WiFi.hostByName(ntpServerName, timeServerIP);
+
+
+
+        int cb;
+        for(int i = 0; i < 3; i++){
+            memset(packetBuffer, 0, NTP_PACKET_SIZE);
+            packetBuffer[0] = 0b11100011;
+            packetBuffer[1] = 0;
+            packetBuffer[2] = 6;
+            packetBuffer[3] = 0xEC;
+            packetBuffer[12] = 49;
+            packetBuffer[13] = 0x4E;
+            packetBuffer[14] = 49;
+            packetBuffer[15] = 52;
+            ntpUDP.beginPacket(timeServerIP, 123);                       //NTP порт 123
+            ntpUDP.write(packetBuffer, NTP_PACKET_SIZE);
+            ntpUDP.endPacket();
+            delay(800);                                                  // чекаємо пів секуни
+            cb = ntpUDP.parsePacket();
+            if(!cb) Serial.println("          no packet yet..." + String (i + 1));
+            if(!cb && i == 2) {                                          // якщо час не отримано
+            statusUpdateNtpTime = 0;
+            return;                                                    // вихіз з getNTPtime()
+            }
+            if(cb) i = 3;
+        }
+
+        if(cb) {                                                      // якщо отримали пакет з серверу
+            ntpUDP.read(packetBuffer, NTP_PACKET_SIZE);
+            unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+            unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+            unsigned long secsSince1900 = highWord << 16 | lowWord;
+            const unsigned long seventyYears = 2208988800UL;        // Unix час станом на 1 січня 1970. в секундах, то 2208988800:
+            unsigned long epoch = secsSince1900 - seventyYears;
+            boolean summerTime;
+
+            if(month < 3 || month > 10) summerTime = false;             // не переходимо на літній час в січні, лютому, листопаді і грудню
+            if(month > 3 && month < 10) summerTime = true;              // Sommerzeit лічимо в квіні, травні, червені, липні, серпені, вересені
+            if(((month == 3) && (hour + 24 * day)) >= (3 + 24 * (31 - (5 * year / 4 + 4) % 7)) || ((month == 10) && (hour + 24 * day)) < (3 + 24 * (31 - (5 * year / 4 + 1) % 7))) summerTime = true;
+            epoch += (int)(timeZone*3600 + (3600*(summertime   /*isDayLightSaving*/ && summerTime)));
+
+            g_year = 0;
+            int days = 0;
+            uint32_t time;
+            time = epoch/86400;
+            g_hour = (epoch % 86400L) / 3600;
+            g_minute = (epoch % 3600) / 60;
+            g_second = epoch % 60;
+            g_dayOfWeek = (((time) + 4) % 7) + 1;
+            while((unsigned)(days += (LEAP_YEAR(g_year) ? 366 : 365)) <= time) {    // Счет года
+            g_year++;
+            }
+            days -= LEAP_YEAR(g_year) ? 366 : 365;
+            time -= days;
+            days = 0;
+            g_month = 0;
+            uint8_t monthLength = 0;
+            for(g_month = 0; g_month < 12; g_month++){                      // Счет месяца
+            if(g_month == 1){
+                if(LEAP_YEAR(g_year)) monthLength = 29;
+                else monthLength = 28;
+            }
+            else monthLength = monthDays[g_month];
+            if(time >= monthLength) time -= monthLength;
+            else break;
+            }
+            g_month++;
+            g_day = time + 1;
+            g_year += 1970;
+            return;
+        }
+        DEBUG("Nie ma czasu(((");
+    }
